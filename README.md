3ad911f6072ad060be50ce4ad61af387e78ebfce
James Berg <james.berg38@yahoo.com>
2020-03-26 14:58:36 -0500

    Finished project. And yes, I realized just now I haven't been doing commits!

--- README.md
+++ README.md
@@ -1,105 +1,61 @@
-# Unit 09 Node.js and ES6+ Homework: Good README Generator
+![JamesBerg1956](https://avatars0.githubusercontent.com/u/59320072?v=4)
+# code-quiz
 
-Create a command-line application that dynamically generates a README.md from a user's input. The application will be invoked with the following command:
+## Table of Contents
 
-```sh
-node index.js
-```
+[Description](./README.md#Description)
 
-The user will be prompted for their GitHub username, which will be used to make a call to the GitHub API to retrieve their email and profile image. They will then be prompted with questions about their project.
+[Installation](./README.md#Installation)
 
-The README will be populated with the following:
+[Usage](./README.md#Usage)
 
-* At least one badge
-* Project title
-* Description
-* Table of Contents
-* Installation
-* Usage
-* License
-* Contributing
-* Tests
-* Questions
-  * User GitHub profile picture
-  * User GitHub email
+[Licensing](./README.md#Licensing)
 
-Following the [common templates for user stories](https://en.wikipedia.org/wiki/User_story#Common_templates), we can frame this challenge as follows:
+[Contributing](./README.md#Contributing)
 
-```
-AS A developer
+[Testing](./README.md#Testing)
 
-I WANT a README generator
-
-SO THAT I can easily put together a good README for a new project
-```
-
-Refer to the [Good README guide](../../01-HTML-Git-CSS/04-Supplemental/Good-README-Guide/README.md).
-
-## Business Context
-
-When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly. This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
-
-## Minimum Requirements
-
-* Functional, deployed application.
-
-* GitHub repository with a unique name and a README describing project.
-
-* The generated README includes a bio image from the user's GitHub profile.
-
-* The generated README includes the user's email.
-
-* The generated README includes the following sections: 
-  * Title
-  * Description
-  * Table of Contents
-  * Installation
-  * Usage
-  * License
-  * Contributing
-  * Tests
-  * Questions
-
-* The generated README includes 1 badge that's specific to the repository.
-
-```
-GIVEN the developer has a GitHub profile and a repository
-
-WHEN prompted for the developer's GitHub username and repo specific information
-
-THEN a README for the repo is generated
-```
-- - -
-
-## Commit Early and Often
-
-One of the most important skills to master as a web developer is version control. Building the habit of committing via Git is important for two reasons:
-
-* Your commit history is a signal to employers that you are actively working on projects and learning new skills.
-
-* Your commit history allows you to revert your code base in the event that you need to return to a previous state.
-
-Follow these guidelines for committing:
-
-* Make single-purpose commits for related changes to ensure a clean, manageable history. If you are fixing two issues, make two commits.
-
-* Write descriptive, meaningful commit messages so that you and anyone else looking at your repository can easily understand its history.
-
-* Don't commit half-done work, for the sake of your collaborators (and your future self!).
-
-* Test your application before you commit to ensure functionality at every step in the development process.
-
-We would like you to have more than 200 commits by graduation, so commit early and often!
-
-## Submission on BCS
-
-You are required to submit the following:
-
-* An animated GIF demonstrating the app functionality
-
-* A generated README.md file for a project repo.
-
-* The URL of the GitHub repository
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+[Questions](./README.md#Questions)
+## Description
+                        
+This project automatically creates a README.md file
+## Installation
+                        
+1. Clone the repo
+                        
+`git clone https://github.com/JamesBerg1956/code-quiz.git`
+                        
+2. Install npm packages
+                        
+`npm install`
+## Usage
+                        
+This is useful whenever starting a new GitHub repo
+## Licensing
+                        
+This project is licensed under lgpl-3.0.
+## Contributing
+                        
+###### Step 1
+                        
+* Option 1
+                        
+  - Fork this repository
+                        
+* Option 2
+                        
+  - Clone this repository `git clone https://github.com/JamesBerg1956/code-quiz.git`
+                        
+###### Step 2
+                        
+* Make local changes
+                        
+###### Step 3
+                        
+* Create a new pull request using `https://github.com/JamesBerg1956/code-quiz/compare/`
+## Testing
+                        
+Testing was performed manually
+## Questions
+                        
+You can reach me for questions at: jamesberg38@yahoo.com
--- index.js
+++ index.js
@@ -1,12 +1,224 @@
-const questions = [
+// import inquirer
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const axios = require("axios");
 
+const questions = 
+[
+    
 ];
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//create var to store profile url
+var strGitHubProfileURL = "";
+var strGitHubUserName = "";
+
+//create empty array of repo names
+var arrRepoNames = [];
+//create an array of license choices
+const arrLicenses = 
+[
+    "apache-2.0","bsd-3-clause","cc-by-4.0","epl-1.0","gpl-3.0","lgpl-3.0","ms-pl","mit","mpl-2.0","osi-3.0","unlicense"
+]
+
 
 function writeToFile(fileName, data) {
 }
 
+// START init() function
 function init() {
 
+    // Call enquirer to get GitHub Username
+    inquirer
+        // START inquirer prompt
+        .prompt([
+            {
+                type: "input",
+                message: "Enter a GitHub Username.",
+                name: "UserName"
+            }
+        ])
+        // END inquirer prompt
+
+        // START Axios call
+        .then(function({UserName}){
+            
+            strGitHubUserName = UserName;
+
+            //create queryString
+            var queryString = `https://api.github.com/users/${strGitHubUserName}/repos?per_page=100`
+            
+            //call axios
+            axios
+            // URL for API call
+            .get(queryString)
+
+            // START axios promise
+            .then(function(response){
+                
+                //create var to store repos
+                var arrRepos = response.data;
+                
+                //loop through repo array
+                arrRepos.forEach(element => {
+
+                    // add repo name to array
+                    arrRepoNames.push(element.name);
+
+                    //get strGitHubProfileURL
+                    strGitHubProfileURL = element.owner.avatar_url;
+
+                })
+    
+            })
+            // END axios promise
+            // START second inquirer prompt
+            .then(function(){
+                // TODO: add questions from object array
+                inquirer
+                    .prompt([
+                       {
+                           type: "list",
+                           choices: arrRepoNames,
+                           message: "Select the repository for this project",
+                           name: "Repo"
+                       },
+                       {
+                           type: "input",
+                           message: "Enter a description of what the project does",
+                           name: "Description"
+                       },
+                       {
+                           type: "input",
+                           message: "Enter what the project is useful for",
+                           name: "Usage"
+                       },
+                       {
+                           type: "list",
+                           choices: arrLicenses,
+                           message: "Select the license for the project",
+                           name: "License"
+                       },
+                       {
+                           type: "input",
+                           message: "Describe how you performed testing on the project",
+                           name: "Testing"
+                       },
+                       {
+                           type: "input",
+                           message: "Enter an email address where user can contact you",
+                           name: "Contact"
+                       }
+                    ])
+                    // START readme.md creation
+                    .then(function({Repo, Description, Usage, License, Testing, Contact}){
+                        
+                        // write title header section
+                        // TODO: include GitHub profile picture
+                        var strTitle =
+                        `![JamesBerg1956](${strGitHubProfileURL})\n# ${Repo}`;
+                        
+                        // write table of contents section
+                        var strTableOfContents = 
+                        `\n\n## Table of Contents\n\n[Description](./README.md#Description)\n\n[Installation](./README.md#Installation)\n\n[Usage](./README.md#Usage)\n\n[Licensing](./README.md#Licensing)\n\n[Contributing](./README.md#Contributing)\n\n[Testing](./README.md#Testing)\n\n[Questions](./README.md#Questions)`;
+                        
+                        // write description section
+                        var strDescription = 
+                        `\n## Description
+                        \n${Description}`;
+                        
+                        // write installation section
+                        var strInstallation = 
+                        `\n## Installation
+                        \n1. Clone the repo
+                        \n\`git clone https://github.com/${strGitHubUserName}/${Repo}.git\`
+                        \n2. Install npm packages
+                        \n\`npm install\``;
+                        
+                        // write usage section
+                        var strUsage =
+                        `\n## Usage
+                        \n${Usage}`;
+                        
+                        // Write license section
+                        var strLicense = 
+                        `\n## Licensing
+                        \nThis project is licensed under ${License}.`;
+                        
+                        // TODO: add license badge
+
+                        // write contributing section
+                        var strContributing = 
+                        `\n## Contributing
+                        \n###### Step 1
+                        \n* Option 1
+                        \n  - Fork this repository
+                        \n* Option 2
+                        \n  - Clone this repository \`git clone https://github.com/${strGitHubUserName}/${Repo}.git\`
+                        \n###### Step 2
+                        \n* Make local changes
+                        \n###### Step 3
+                        \n* Create a new pull request using \`https://github.com/${strGitHubUserName}/${Repo}/compare/\``;
+                        
+                        // Write the testing section
+                        var strTesting =
+                        `\n## Testing
+                        \n${Testing}`;
+                        
+                        // Write the contact section
+                        // TODO: format Contact as email link
+                        var strContact =
+                        `\n## Questions
+                        \nYou can reach me for questions at: ${Contact}`;
+                        
+                        //concat readme content
+                        var strReadmeContent = `${strTitle}${strTableOfContents}${strDescription}${strInstallation}${strUsage}${strLicense}${strContributing}${strTesting}${strContact}`;
+                        
+                        writeFileAsync("README.md", strReadmeContent)
+                        .then(function(data){
+                            console.log("README.md succesfully created");
+                        })
+                        .catch(function(err){
+                            if(err){
+                                console.log("writeFileAsync error: "+ err);
+                            }
+                        });
+                                                
+                    })
+                    // END readme.md creation
+                    // START document writer catch
+                    .catch(function(err){
+                        if(err){
+                            console.log("document writer catch: "+err);
+                        }
+                    });
+                    // END document writer catch
+
+            })
+            // END second inquirer prompt
+
+            // START axios catch
+            .catch(function(err){
+                //error handling for axios
+                if(err){
+                    console.log("Axios catch: "+err);
+                }
+            });
+            // END axios catch
+
+        })
+        // START inquirer catch
+        .catch(function(err){
+            //error handling for inquirer
+            if(err){
+                console.log("Inquirer catch 1: "+err);
+            }
+        });
+        // END inquirer catch
+
 }
+// END init() function
 
 init();
